c3bd9f38ff0a4e4a0170a4cc29e0b706acbca687
DESKTOP-NQCQC97\seek <seekchange@daum.net>
2022-09-20 19:55:28 +0900

    과제1 리팩토링3

--- Assignment1/Assignment1/BigNumberCalculator.cs
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -294,7 +294,7 @@ namespace Assignment1
 
                 for (int i = 0; i < chars.Length; i++)
                 {
-                    string temp = chars[i] == '0' ? sum : addPositiveDecimalStrings(sum, $"{chars[i]}");
+                    string temp = chars[i] == '0' ? sum : addTwoDecimalStrings(sum, $"{chars[i]}");
 
                     if (i == chars.Length - 1)
                     {
@@ -302,7 +302,7 @@ namespace Assignment1
                         break;
                     }
 
-                    sum = addPositiveDecimalStrings(temp, temp);
+                    sum = addTwoDecimalStrings(temp, temp);
                 }
 
                 if (bMinus)
@@ -543,7 +543,7 @@ namespace Assignment1
             return result;
         }
 
-        private static string addPositiveDecimalStrings(string A, string B)
+        private static string addTwoDecimalStrings(string A, string B)
         {
             bool bCarriedOver = false;
 
